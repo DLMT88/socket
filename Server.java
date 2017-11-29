2450f03cbc7dc055f689b2737a3f29f6be2d7b3d
DLMT88 <33481266+DLMT88@users.noreply.github.com>
2017-11-29 10:43:15 +0100

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,38 +1,31 @@
-package servertcpstr;
+package serverstr;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
-public class ServerTcpStr {  
+public class ServerStr
+{
   ServerSocket server      = null;
   Socket client            = null;
   String stringaRicevuta   = null;
-  String stringaModificata = null;  
+  String stringaModificata = null;
   BufferedReader inDalClient;
   DataOutputStream outVersoClient;
 
-    public static void main(String[] args) {
-
-
-  ServerTcpStr servente = new ServerTcpStr();
-  servente.attendi();
-  servente.comunica();
-}
-    
-public Socket attendi(ServerSocket server, Socket client, BufferedReader inDalClient, DataOutputStream outVersoClient)
+public Socket attendi ()
 {
   try
   {
    System.out.println("1 SERVER partito in esecuzione...");
-   server = new ServerSocket(6789);
+   server = new ServerSocket(6781);
    client = server.accept();
    server.close();
-inDalClient= new BufferedReader(new InputStreamReader(client.getInputStream()));
+   inDalClient = new BufferedReader(new InputStreamReader (client.getInputStream()));
    outVersoClient = new DataOutputStream(client.getOutputStream());
   }
 
-catch (IOException e)
+catch (Exception e)
 {
  System.out.println(e.getMessage());
  System.out.println("Errore durante l'istanza del server!");
@@ -45,9 +38,9 @@ public void comunica ()
 {
  try
  {
-  System.out.println("3 Benvenuto client, scrivi una frase e la trasformo in "
-          + "maiuscolo. Attendo...");
-  System.out.println("6 Ricevuta la stringa dal cliente:" + stringaRicevuta);
+  System.out.println("3 Benvenuto client, scrivi una frase e la trasformo in maiuscolo. Attendo...");
+  stringaRicevuta = inDalClient.readLine();
+  System.out.println("6 Ricevuta la stringa dal cliente:" +stringaRicevuta);
  
   stringaModificata=stringaRicevuta.toUpperCase();
   System.out.println("7 Invio la stringa modificata al client...");
@@ -58,24 +51,10 @@ public void comunica ()
  }
  catch(IOException e){}
 }
-     // Copia del metodo attendi
-    private Socket attendi() {
-        try
-  {
-   System.out.println("1 SERVER partito in esecuzione...");
-   server = new ServerSocket(6789);                                             //porta 
-   client = server.accept();
-   server.close();
-inDalClient= new BufferedReader(new InputStreamReader(client.getInputStream()));
-   outVersoClient = new DataOutputStream(client.getOutputStream());
-  } 
 
-catch (IOException e)
-{
- System.out.println(e.getMessage());
- System.out.println("Errore durante l'istanza del server!");
- System.exit(1);
+public static void main(String args[]) {
+  ServerStr servente = new ServerStr();
+  servente.attendi ();
+  servente.comunica();
 }
-return client;
-    }
-  } 
+}
